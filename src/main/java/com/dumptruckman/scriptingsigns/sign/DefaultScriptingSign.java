8211f47a1a4081792feda8ab0984d66feaae5098
Jeremy Wood <farachan@gmail.com>
2012-01-30 17:39:04 -0500

    Gotta figure out how to update submodule.

--- src/main/java/com/dumptruckman/scriptingsigns/sign/DefaultScriptingSign.java
+++ src/main/java/com/dumptruckman/scriptingsigns/sign/DefaultScriptingSign.java
@@ -1,11 +1,12 @@
 package com.dumptruckman.scriptingsigns.sign;
 
+import com.dumptruckman.actionmenu2.api.MenuHandle;
 import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 
-public class DefaultScriptingSign {
+public class DefaultScriptingSign implements ScriptingSign {
     
-    protected DefaultScriptingSign(Sign sign, Player owner) {
+    protected DefaultScriptingSign(Sign sign, Player owner, MenuHandle menuHandle) {
 
     }
 }
--- src/main/java/com/dumptruckman/scriptingsigns/sign/Signs.java
+++ src/main/java/com/dumptruckman/scriptingsigns/sign/Signs.java
@@ -1,5 +1,8 @@
 package com.dumptruckman.scriptingsigns.sign;
 
+import com.dumptruckman.actionmenu2.api.MenuHandle;
+import com.dumptruckman.actionmenu2.api.Menus;
+import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 
@@ -8,7 +11,12 @@ public class Signs {
     private Signs() { }
     
     public static ScriptingSign newSign(Sign sign, Player owner) {
-
+        MenuHandle handle = Menus.getMenuHandle(sign);
+        handle.getView().setMenu(handle.getMenu());
+        return new DefaultScriptingSign(sign, owner, handle);
+    }
+    
+    public static ScriptingSign getSign(Block block) {
         return null;
     }
 }
